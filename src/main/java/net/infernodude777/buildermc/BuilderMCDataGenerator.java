1067e86f5fafe6b5b4da6605a7b3f95cb68ae94e
Nikhil Nagaraj <nikhil.p.nagaraj@gmail.com>
2026-07-25 11:11:16 -0700

    first commit

---
+++ src/main/java/net/infernodude777/buildermc/BuilderMCDataGenerator.java
@@ -0,0 +1,11 @@
+package net.infernodude777.buildermc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BuilderMCDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
